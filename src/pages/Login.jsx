dce9eade0a5141256f5a88415294926372917bfa
Ilham Azhar <ilham.azhar@delameta.com>
2023-11-17 21:03:04 +0700

    Create page & function for register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,37 @@
+import { Link } from 'react-router-dom';
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="max-w-lg mx-auto p-3">
+      <h1 className="text-3xl text-center font-semibold uppercase my-7">
+        login
+      </h1>
+      <form className="flex flex-col gap-3">
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="border rounded-lg p-3"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="border rounded-lg p-3"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 disabled:opacity-80">
+          Login
+        </button>
+      </form>
+      <div className="flex gap-2 mt-3">
+        <p>Don&apos;t have an account?</p>
+        <Link to="/register">
+          <span className="text-blue-600">Register </span>
+          <span>here!</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,100 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const apiUrl = '/api/users';
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiRDNsNG1ldDQiLCJyb2xlIjoic3VwZXItYWRtaW4ifSwiaWF0IjoxNzAwMjI2ODUyLCJleHAiOjE3MDAzMTMyNTJ9.IhZfPI20b8saekqN4alzYlDT6glnxQZVUAT4fAV-VqM';
+
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    };
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(formData),
+      }).then(setLoading(true));
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.errors);
+      }
+
+      navigate('/login');
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="max-w-lg mx-auto p-3">
+      <h1 className="text-3xl text-center font-semibold uppercase my-7">
+        user registration
+      </h1>
+      {error && <small className="text-red-500">{error}</small>}
+      <form
+        className="flex flex-col gap-3 my-2"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          id="name"
+          placeholder="Name"
+          className="border rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="border rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="border rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <button
+          className="bg-slate-700 text-white rounded-lg p-3 disabled:opacity-80"
+          disabled={loading}
+        >
+          {loading ? 'Loading...' : 'Register'}
+        </button>
+      </form>
+      <div className="flex gap-2">
+        <p>Have an account?</p>
+        <Link to="/login">
+          <span className="text-blue-600">Login </span>
+          <span>here!</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
